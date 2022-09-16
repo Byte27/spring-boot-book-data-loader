f44c764e9eba075455478c02b71f407031ce8fff
Dries Elliott <dries.elliott@clarivate.com>
2022-09-16 17:59:28 +0200

    Fixes failing ApplicationContext loading test
    Configures an in-memory database for tests

--- src/main/java/com/byte27/dataloader/SpringBootBookDataLoaderApplication.java
+++ src/main/java/com/byte27/dataloader/SpringBootBookDataLoaderApplication.java
@@ -1,10 +1,8 @@
 package com.byte27.dataloader;
 
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
 
 @SpringBootApplication
 public class SpringBootBookDataLoaderApplication implements CommandLineRunner {
