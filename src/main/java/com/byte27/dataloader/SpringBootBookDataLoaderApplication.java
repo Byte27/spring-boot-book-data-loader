3e6112ac7478ce5b5d1f6e70c64ae30317839894
Dries Elliott <dries.elliott@clarivate.com>
2022-09-16 15:28:26 +0200

    Creates initial Spring Boot application
    Used https://start.spring.io/ to generate the project skeleton.

---
+++ src/main/java/com/byte27/dataloader/SpringBootBookDataLoaderApplication.java
@@ -0,0 +1,13 @@
+package com.byte27.dataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBookDataLoaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBookDataLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byte27/dataloader/SpringBootBookDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byte27.dataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBookDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
