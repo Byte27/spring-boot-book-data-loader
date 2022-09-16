be5c161f479a7860628959b44f8053c894bd29e8
Dries Elliott <dries.elliott@clarivate.com>
2022-09-16 17:29:59 +0200

    Sets up Liquibase to create a 'books' table

--- src/main/java/com/byte27/dataloader/SpringBootBookDataLoaderApplication.java
+++ src/main/java/com/byte27/dataloader/SpringBootBookDataLoaderApplication.java
@@ -1,13 +1,20 @@
 package com.byte27.dataloader;
 
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.sql.init.dependency.DependsOnDatabaseInitialization;
 
 @SpringBootApplication
-public class SpringBootBookDataLoaderApplication {
+public class SpringBootBookDataLoaderApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootBookDataLoaderApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
